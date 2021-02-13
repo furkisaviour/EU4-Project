c3c417a6b06aa07311ac9091523e5cec3d09dd57
furkisaviour <furkankayakho@gmail.com>
2021-02-13 15:41:01 +0300

    project is already in remote repo , committing more changes

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
-        //more stuff
-        //Mac user and windows users are happy
+    //more stuff
+    //Mac user and windows users are happy
 
+    //I have already published on Github.com
+    //now i am ready to make more commits
+    //but do i need to share
+    //Absolutely no
+    //It's time commit and just push!!!
 }
